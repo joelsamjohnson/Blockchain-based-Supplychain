a1b1e4cb5a658447c71d958248e8201fcd18fa9e
Joel Sam Johnson <joelsamueljohnson@gmail.com>
2024-05-05 17:59:32 +0530

    added images and separated contracts

--- supplychain/forms.py
+++ supplychain/forms.py
@@ -19,6 +19,7 @@ class AddProductForm(forms.Form):
     name = forms.CharField(max_length=100)
     description = forms.CharField(max_length=500)
     price = forms.DecimalField(max_digits=10, decimal_places=2)
+    image = forms.ImageField()
 
 
 class LoginForm(forms.Form):
--- supplychain/views.py
+++ supplychain/views.py
@@ -1,5 +1,4 @@
 import os
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
@@ -8,7 +7,7 @@ from django.views.decorators.http import require_http_methods
 from .ethereum_utils import get_web3_connection, get_contract_instance, send_transaction
 from .models import User, Product, Register
 from django.shortcuts import render, redirect
-from django.urls import reverse
+import requests
 from .forms import AddEntityForm, AddProductForm, LoginForm, RegisterForm, CreateUserForm
 
 
@@ -24,24 +23,41 @@ account_from = {
             "address": '0xb30E2F234958fb7A5D4D3D1c08395B81C7a51803',
         }
 
+productcontract_address = '0xE17b12feDa856174ddB941915ca5B817A9706017'
+productabi_filename = 'ProductManagementABI.json'
+productabi_path = os.path.join(contracts_directory, productabi_filename)
+productcontract = get_contract_instance(web3, productcontract_address, productabi_path)
 
 
-def add_entity_to_blockchain(entity_type, address, name, place):
-    try:
-        function_name = f'add{entity_type}'
-        args = [address, name, place]
-        tx_hash = send_transaction(web3, contract, function_name, args, account_from['address'], account_from['private_key'])
-        return {'success': True, 'transaction_hash': tx_hash}
-    except Exception as e:
-        return {'success': False, 'error': str(e)}
+rolecontract_address = '0x315FF1B4Fa64F0aC9eD6aEc30Aa11886E8d92Aeb'
+roleabi_filename = 'RoleManagementABI.json'
+roleabi_path = os.path.join(contracts_directory, roleabi_filename)
+rolecontract = get_contract_instance(web3, rolecontract_address, roleabi_path)
+
+# def add_entity_to_blockchain(entity_type, address, name, place):
+#     try:
+#         function_name = f'add{entity_type}'
+#         args = [address, name, place]
+#         tx_hash = send_transaction(web3, contract, function_name, args, account_from['address'], account_from['private_key'])
+#         return {'success': True, 'transaction_hash': tx_hash}
+#     except Exception as e:
+#         return {'success': False, 'error': str(e)}
     
 
-def add_product_to_blockchain(name, description, price):
+def pin_image_to_ipfs(image_file):
+    url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+    headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkYTk2OTc4ZS02NWJiLTQwOGEtYTAzMS0yZGFhN2ViY2IyMDMiLCJlbWFpbCI6ImpvZWxzYW1qb2huc29uQHlhaG9vLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJiYTViNDljZDljOTUyY2JiNzhlYSIsInNjb3BlZEtleVNlY3JldCI6ImY5ZmIzMDg3MTE1Y2Y2MWVkOTk0YWZiOTVhZjYzNWQwZjgyOGUzOGIzNzlhNGViOGE2ZDhhNGQ4OWI0NzJjYjIiLCJpYXQiOjE3MTQ4ODUyNzd9.LnfSkclSo3E5WygrsMwkVieZGot00R0yBlFkxMWF5Sc"}
+    files = {'file': (image_file.name, image_file, 'multipart/form-data')}
+    response = requests.post(url, files=files, headers=headers)
+    ipfs_hash = response.json().get('IpfsHash')
+    return ipfs_hash
+
+def add_product_to_blockchain(name, description, price, ipfs_hash):
     try:
         function_name = 'addproduct'
-        args = [name, description, price]
+        args = [name, description, price, ipfs_hash]
         # Ensure that `send_transaction` is capable of handling the connection and sending data to the blockchain
-        tx_hash = send_transaction(web3, contract, function_name, args, account_from['address'], account_from['private_key'])
+        tx_hash = send_transaction(web3, productcontract, function_name, args, account_from['address'], account_from['private_key'])
         return {'success': True, 'transaction_hash': tx_hash}
     except Exception as e:
         return {'success': False, 'error': str(e)}
@@ -53,11 +69,12 @@ def add_product(request):
             _name = form.cleaned_data['name']
             _description = form.cleaned_data['description']
             _price = form.cleaned_data['price']
-            # Create a Product instance in Django database
+            _image = request.FILES['image']
+            ipfs_hash = pin_image_to_ipfs(_image)
             Product.objects.create(name=_name, description=_description, price=_price)
 
             # Call blockchain function
-            result = add_product_to_blockchain(_name, _description, _price)
+            result = add_product_to_blockchain(_name, _description, _price, ipfs_hash)
             if result['success']:
                 print(result)
                 return redirect('success_url')  # Make sure 'success_url' is defined in your urls.py
@@ -77,7 +94,7 @@ def product_details_initial(request):
 @require_http_methods(["GET"])
 def show_product_details(request, product_id):
     try:
-        product = contract.functions.ProductStock(product_id).call()
+        product = productcontract.functions.ProductStock(product_id).call()
         stage_descriptions = {
             0: "Product Ordered",
             1: "Manufacturing Stage",
@@ -99,16 +116,15 @@ def show_product_details(request, product_id):
             if user_id == 0:
                 return "Empty", "Empty"  
             elif user_type == 'MAN':
-                userdetails = contract.functions.MAN(user_id).call()
+                userdetails = rolecontract.functions.MAN(user_id).call()
             elif user_type == 'DIS':
-                userdetails = contract.functions.DIS(user_id).call()
+                userdetails = rolecontract.functions.DIS(user_id).call()
             elif user_type == 'RET':
-                userdetails = contract.functions.RET(user_id).call()
+                userdetails = rolecontract.functions.RET(user_id).call()
             else:
                 return "Invalid Type", "Invalid Type"
-            return userdetails[2], userdetails[3]  # Assuming [0] is name and [1] is place
+            return userdetails[2], userdetails[3] 
 
-        # Fetch and add user details to context
         man_id, dis_id, ret_id = product[4], product[5], product[6]
         print(f"Product data from blockchain: {product}")
         print(f"Manufacturer ID: {man_id}")
@@ -133,31 +149,24 @@ def add_entity(request):
             name = form.cleaned_data['name']
             place = form.cleaned_data['place']
             User.objects.create(user_type=entity_type, address=address, name=name, place=place)
-
-            result = add_entity_to_blockchain(entity_type, address, name, place)
-            if result['success']:
-                print(result)
-                return redirect('success_url')
-            else:
-                pass
     else:
         form = AddEntityForm()
         man = {}
-        man_ctr = contract.functions.manCtr().call()
+        man_ctr = rolecontract.functions.manCtr().call()
         for i in range(man_ctr):
-            man[i] = contract.functions.MAN(i + 1).call()
+            man[i] = rolecontract.functions.MAN(i + 1).call()
             print(man[i])
 
         dis = {}
-        dis_ctr = contract.functions.disCtr().call()
+        dis_ctr = rolecontract.functions.disCtr().call()
         for i in range(dis_ctr):
-            dis[i] = contract.functions.DIS(i + 1).call()
+            dis[i] = rolecontract.functions.DIS(i + 1).call()
             print(dis[i])
 
         ret = {}
-        ret_ctr = contract.functions.retCtr().call()
+        ret_ctr = rolecontract.functions.retCtr().call()
         for i in range(ret_ctr):
-            ret[i] = contract.functions.RET(i + 1).call()
+            ret[i] = rolecontract.functions.RET(i + 1).call()
             print(ret[i])
 
         return render(request, 'register_user.html', {
